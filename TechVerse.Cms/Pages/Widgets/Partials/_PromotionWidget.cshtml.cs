008452614e278b78fd0f7157ad801db6d954ccc8
sinwanwisa <sinwanwisa.jk@gmail.com>
2026-01-04 21:45:21 +0700

    ดึงข้อมูลใน Promotion widget

---
+++ TechVerse.Cms/Pages/Widgets/Partials/_PromotionWidget.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using OrchardCore.ContentManagement;
+
+namespace TechVerse.Cms.Pages.Widgets.Partials
+{
+    public class _PromotionWidgetModel : PageModel
+    {
+        public ContentItem ContentItem { get; set; }
+
+    }
+}
