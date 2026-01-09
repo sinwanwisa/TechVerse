c04bd44b2a97123e3a8f8947e1a7070e6ffd4144
sinwanwisa <sinwanwisa.jk@gmail.com>
2026-01-09 17:56:30 +0700

    Search Widget

---
+++ TechVerse.Cms/Pages/Widgets/Partials/SearchForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TechVerse.Cms.Pages.Widgets.Partials
+{
+    public class SearchFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
