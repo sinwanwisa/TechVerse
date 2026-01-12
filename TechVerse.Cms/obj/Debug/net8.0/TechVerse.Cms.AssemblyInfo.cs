5709b7499bcc5b88d6450b20b1e591a15813f9e7
sinwanwisa <sinwanwisa.jk@gmail.com>
2026-01-12 15:21:26 +0700

    Add 30 Article

--- TechVerse.Cms/obj/Debug/net8.0/TechVerse.Cms.AssemblyInfo.cs
+++ TechVerse.Cms/obj/Debug/net8.0/TechVerse.Cms.AssemblyInfo.cs
@@ -110,7 +110,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechVerse.Cms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f83c829af1893e2d1a0c8777868322380fc0186")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+965457d535e80d121e2863a019cd1eaafa855fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechVerse.Cms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechVerse.Cms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
